84b49917c377a0d626b4d07129f997088109a364
satvirgrewal <satvir.grewal03@gmail.com>
2017-05-21 15:38:33 -0500

    Another solution

--- src/chapter1ArraysAndStrings/Problem1_1CheckStringHasUniqueCharacters.java
+++ src/chapter1ArraysAndStrings/Problem1_1CheckStringHasUniqueCharacters.java
@@ -1,25 +1,24 @@
 package chapter1ArraysAndStrings;
 
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Problem1_1CheckStringHasUniqueCharacters {
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-
-		boolean[] bool = new boolean[256];
-		String string = scan.nextLine();
-		for (int i=0; i<string.length();i++){
-			int a = string.charAt(i);
-			if(bool[a]&& a!=32) {
-				System.out.println("String has duplicate characters");
-				return;
+		Scanner input = new Scanner(System.in);
+		String ip = input.nextLine();
+		
+		boolean[] counter = new boolean[256];
+		String duplicate = "unique";
+		for(int i=0;i<ip.length();i++){
+			if(counter[ip.charAt(i)]==true){
+				duplicate = "repeated";
+				break;
 			}
-			bool[a] = true;
+			else{
+				counter[ip.charAt(i)]=true;
+			}
+			
 		}
-
-		System.out.println("String has unique characters");
-		scan.close();
+		System.out.println("String contains " +duplicate + " characters");
 	}
 }
